483de6cad0b5bd4332d1604ea807580b3329f688
Tram Nguyen <24tnguyen.2003@gmail.com>
2024-09-27 01:40:38 -0700

    Create run_program.sh

---
+++ run_program.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Grant permission
+chmod +x run_program.sh
+
+# Execute lexical anlyzer
+echo "~~~ Running Lexical Analyzer... ~~~\n"
+python3 main.py
+echo "\n=====================================================================================================\n"
+
+# Execute test cases
+echo "~~~ Running Test Cases... ~~~\n"
+python3 test.py
